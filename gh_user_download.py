d1998114771e451def027004134b644179f1b934
Jack McCracken <jackamc19@gmail.com>
2014-08-02 06:32:55 -0400

    Finished script. Added docopt.

--- gh_user_download.py
+++ gh_user_download.py
@@ -4,8 +4,29 @@ Usage:
   gh_user_download -h | --help
 """
 
+from __future__ import print_function
+
+import os
+
 from pygithub3 import Github
+from docopt import docopt
 
-gh = Github()
 
-print gh.repos.list('JackMc').all()
+def main():
+    arguments = docopt(__doc__, version="testing")
+
+    who = arguments['<who>']
+    where = arguments['<where>']
+
+    gh = Github()
+
+    repos = gh.repos.list(who).all()
+
+    for repo in repos:
+        url = repo.git_url
+        print(url, 'to', os.path.join(where, repo.name))
+        os.system('git clone ' + url + ' ' + os.path.join(where, repo.name))
+
+
+if __name__ == '__main__':
+    main()
